b3c299eaf2274b815ab0f102d45f7511796053ef
avivilloz <avivilloz@gmail.com>
2026-06-09 17:22:16 +0300

    updating repository

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-cd /home/avivilloz/git/avivkilloz/deckyfin 
+DECKYFIN_REPO_PATH=/home/deck/git/github/avivkilloz/deckyfin
+DECKIFIN_PLUGIN_PATH=/home/deck/homebrew/plugins/deckyfin
+
+cd $DECKYFIN_REPO_PATH
 git pull
 npm install
 npm run build
-sudo rm -rf /home/avivilloz/homebrew/plugins/deckyfin
-sudo cp -r . /home/avivilloz/homebrew/plugins/deckyfin
+sudo rm -rf $DECKIFIN_PLUGIN_PATH
+sudo cp -r . $DECKIFIN_PLUGIN_PATH
 sudo systemctl restart plugin_loader.service
